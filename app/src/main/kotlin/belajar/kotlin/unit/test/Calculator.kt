e10a1a7cbbca3225e5a75429d1583304866db707
giannurwana19 <giannurwana19@gmail.com>
2023-12-20 20:42:07 +0700

    menggunakan Assertions

--- app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
+++ app/src/main/kotlin/belajar/kotlin/unit/test/Calculator.kt
@@ -4,4 +4,12 @@ class Calculator {
     fun add(first: Int, second: Int): Int {
         return first + second
     }
+
+    fun divide(first: Int, second: Int): Int {
+        if (second == 0) {
+            throw IllegalArgumentException("Can not divided by zero")
+        } else {
+            return first / second
+        }
+    }
 }
--- app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/CalculatorTest.kt
@@ -1,6 +1,8 @@
 package belajar.kotlin.unit.test
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 
 class CalculatorTest {
@@ -8,11 +10,27 @@ class CalculatorTest {
 
     @Test
     fun testAddSuccess() {
-        val result = calculator.add(10, 10);
+        val result = calculator.add(10, 20)
+        Assertions.assertEquals(30, result, "Hasil harusnya 20")
     }
 
     @Test
     fun testAddSuccess2() {
-        val result = calculator.add(10, 10);
+        val result = calculator.add(20, 20)
+        Assertions.assertEquals(40, result, "Hasil harusnya 40")
+    }
+
+    @Test
+    fun testDivideSuccess() {
+        val result = calculator.divide(100, 10)
+
+        Assertions.assertEquals(10, result)
+    }
+
+    @Test
+    fun testDividedFailed() {
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(100, 0)
+        }
     }
 }
