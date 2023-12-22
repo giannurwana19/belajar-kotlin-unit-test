32dcb317ab6aeb3ba6541edd387dbd5f9ab4080f
giannurwana19 <giannurwana19@gmail.com>
2023-12-22 19:38:11 +0700

    siklus hidup test

--- app/src/test/kotlin/belajar/kotlin/unit/test/OrderTest.kt
+++ app/src/test/kotlin/belajar/kotlin/unit/test/OrderTest.kt
@@ -1,25 +1,54 @@
 package belajar.kotlin.unit.test
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestMethodOrder
 
-// by default diurutkan berdasarkan alfanumber atau methodName
+/**
+ * by default diurutkan berdasarkan alfanumber atau methodName
+ * kita bisa mengubah test instance nya menjadi per class
+ *
+ */
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation::class)
 class OrderTest {
+    private var counter = 0
+
+    @BeforeAll
+    fun beforeAll() {
+        println("before all")
+    }
+
+    @AfterAll
+    fun afterAll() {
+        println("after all")
+    }
+
     @Test
     @Order(3)
     fun testA() {
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 
     @Test
     @Order(1)
     fun testC() {
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 
     @Order(2)
     @Test
     fun testB() {
+        counter++
+        println(counter)
+        println(this.hashCode())
     }
 }
