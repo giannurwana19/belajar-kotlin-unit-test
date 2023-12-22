ebecb5efca39dc8651edf0cc9ec714c0af50ce81
giannurwana19 <giannurwana19@gmail.com>
2023-12-22 19:51:01 +0700

    informasi test

---
+++ app/src/test/kotlin/belajar/kotlin/unit/test/InformationTest.kt
@@ -0,0 +1,21 @@
+package belajar.kotlin.unit.test
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Tags
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInfo
+
+@DisplayName("test with TestInfo")
+class InformationTest {
+    @Test
+    @Tags(value = [Tag("contoh1"), Tag("contoh2")])
+    @DisplayName("Sample test")
+    fun sampleTest(testInfo: TestInfo) {
+        println(testInfo.displayName)
+        println(testInfo.tags)
+        println(testInfo.testClass)
+        println(testInfo.testMethod)
+    }
+
+}
